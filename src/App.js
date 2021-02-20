453a06dd5ec7ef04704161f75ecc7eeb289d38cf
Wasim Akhtar Khan <wasimkhan03150@gmail.com>
2021-02-20 11:44:42 +0530

    Initialised Repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import PersonalDetails from './Components/PersonalDetails';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-header'>  
+        <div className='portfolio'>
+          <PersonalDetails />
+          </div>      
+      </div>
+    </div>
+  );
+}
+
+export default App;
